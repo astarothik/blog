ae5791b46643f9e3638055af0240520115003e13
Mihail klinov <klinov.mikhail@inbox.ru>
2016-12-08 16:46:26 +0400

    fix

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,9 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
@@ -32,6 +35,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
