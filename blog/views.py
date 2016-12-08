f5ecd17fb9b5e417da4dd3ff2a3ec5ffe934827e
Mihail klinov <klinov.mikhail@inbox.ru>
2016-12-08 12:26:08 +0400

    views

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
+
 def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
@@ -10,7 +11,6 @@ def post_new(request):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
