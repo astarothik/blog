749a9f0460346f66a2d151f7eacc6da4f498bbae
Mihail klinov <klinov.mikhail@inbox.ru>
2016-12-08 12:31:16 +0400

    views

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
-
 def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
