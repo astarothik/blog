8d6e99d18e79ae753936b510a9bfa630e9b4233b
Mihail klinov <klinov.mikhail@inbox.ru>
2016-12-09 16:59:51 +0400

    sub

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def settings_list(request):
     return render(request, 'blog/base.html', {'settings_page': settings_page})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
